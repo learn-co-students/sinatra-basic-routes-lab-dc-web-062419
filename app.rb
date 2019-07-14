41419b19c3563901937adbe525299012144b7eea
Michelle Lam <shelly2.lam@gmail.com>
2019-07-14 10:57:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Michelle"
+    end
+
+    get '/hometown' do
+        "My hometown is Eau Claire!"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is What does the fox say?"
+    end
 end
