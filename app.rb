14115ec6e1a379ebefacb7e3178bc2c207b37804
SkaMateria <skamateria@gmail.com>
2019-07-15 15:53:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is ____"
+    end
+
+    get '/hometown' do 
+        "My hometown is _____"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is ______"
+    end
 end
